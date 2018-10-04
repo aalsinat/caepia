8f3f992a0d0cd2047329f7fc4d767c8c0f349710
Pedro <pedro.esteban@breadandonion.es>
2018-10-04 23:31:36 +0200

    add new fields in the Product model

--- infra/application/rest-api/src/main/java/com/caepia/app/api/model/domain/Product.java
+++ infra/application/rest-api/src/main/java/com/caepia/app/api/model/domain/Product.java
@@ -22,6 +22,18 @@ public class Product {
     @Column(name = "PK_Product")
     private Integer id;
 
+    @Column(name = "ProductName")
+    private String productName;
+
+    @Column(name = "CostBaseUnits")
+    private Float costBaseUnits;
+
+    @Column(name = "UnitShortName")
+    private String unitShortName;
+
+    @Column(name = "CostBaseUnitsFormat")
+    private String costBaseUnitsFormat;
+
     @Column(name = "swBookmark")
     private Integer isBookmarked;
 
